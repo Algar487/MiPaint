03b81b5811529025304925d1594f791eeefb6239
alejandro.ruiga <d12ruiga@gmail.com>
2021-04-06 20:01:30 +0200

    20210405

--- app/src/main/java/com/ruizgarcia/mipaint/PantallaDibujo.java
+++ app/src/main/java/com/ruizgarcia/mipaint/PantallaDibujo.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 //Clase que gestiona la actividad principal de la aplicación. En este caso es muy sencilla
 public class PantallaDibujo extends AppCompatActivity {
 
+    private PaintView paintView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.pantalla_dibujo);
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
     }
+
 
 
 }
